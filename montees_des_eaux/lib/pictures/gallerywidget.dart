d7dc788e864ca18c37af6004bd803a2a3bfebb2d
LordRayd <samuel-leb@hotmail.fr>
2020-12-28 13:49:24 +0100

    gallery image presque fini

--- montees_des_eaux/lib/pictures/gallerywidget.dart
+++ montees_des_eaux/lib/pictures/gallerywidget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:montees_des_eaux/pictures/photowidget.dart';
 
 class GalleryWidget extends StatefulWidget {
   @override
@@ -6,10 +8,73 @@ class GalleryWidget extends StatefulWidget {
 }
 
 class _GalleryWidgetState extends State<GalleryWidget> {
+
+  List<PhotoWidget> galleryItems = new List<PhotoWidget>();
+  
+  Widget _buildGrid() => GridView.extent(
+    maxCrossAxisExtent: 150,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(galleryItems.length),
+  );
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(
+      child: InkWell(
+        child: Image.network(galleryItems[i].url),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => galleryItems[i]),
+          );
+        },
+      )
+    )
+  );
+    
+  _noFavoriteText(){
+    return Center(
+      child: Text(
+        "Vous n'avez définis aucune photo comme favorie",
+      )
+    );
+  }
+
+  _loadFavoritesPictures() async{
+    var photos = await Hive.openBox('favoritePhoto');
+    for(int i=0; i<photos.length; i++){
+      galleryItems.add(PhotoWidget(url: photos.getAt(i)['url'], hotspotID: photos.getAt(i)['hotspotID']) );
+    }
+  }
+
+  _body(){
+    return FutureBuilder<dynamic>(
+      future: _loadFavoritesPictures(), // function where you call your api
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {  // AsyncSnapshot<Your object type>
+        if( snapshot.connectionState == ConnectionState.waiting){
+            return  Center(child: Text('Chargement des lieux préférés'));
+        } else {
+            if (snapshot.hasError)
+              return Center(child: Text('Error: ${snapshot.error}'));
+            else {
+              if(galleryItems.isEmpty)
+                return _noFavoriteText();
+              else 
+                return _buildGrid();
+            }
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mes lieux favoris"),
+      ),
+      body: _body(),
     );
   }
 }
